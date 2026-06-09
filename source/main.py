5fa8a52ac96aed4fd79bb5f344779ebcf1524180
Marc Solés Rojas <marcsoles27@gmail.com>
2026-06-09 13:13:30 +0200

    intento 3

--- source/main.py
+++ source/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 import sys
 
-linux_mode = 0 # Windows
+linux_mode = 1 # Windows
 if sys.platform.startswith("linux"):
     linux_mode = 1 # Linux
 
@@ -32,12 +32,12 @@ if not api_key:
 
 planner = Planner(event_bus)
 navigation = Navigation("Navigation", event_bus, sensor_data, data_task_bus, shared_data)
-sensory = SensoryModule("Sensory", event_bus, sensor_data, data_task_bus, shared_data)
-human_interaction = HRI("HRI", event_bus, sensor_data, api_key, data_task_bus, shared_data)
+#sensory = SensoryModule("Sensory", event_bus, sensor_data, data_task_bus, shared_data)
+#human_interaction = HRI("HRI", event_bus, sensor_data, api_key, data_task_bus, shared_data)
 data_manager = DataModule("Data", event_bus, data_task_bus, shared_data)
 
-planner.append_modules([navigation, sensory, human_interaction, data_manager])
-
+planner.append_modules([navigation, data_manager])
+#planner.append_modules([navigation, sensory, human_interaction, data_manager])
 if linux_mode:
     ros_module = ROSModule("ROS", event_bus, shared_data)
     planner.append_single_module(ros_module)
@@ -45,8 +45,8 @@ if linux_mode:
 
 planner.start()
 navigation.start()
-sensory.start()
-human_interaction.start()
+#sensory.start()
+#human_interaction.start()
 data_manager.start()
 
 if linux_mode:
